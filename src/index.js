57739a2725c7295d2c1d10238ef48340dc8740da
Anmol Sharma <76692804+D3N2-A@users.noreply.github.com>
2022-07-05 10:58:31 +0530

    ranodm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+import reactDOM from "react-dom";
+const name = "Anmol";
+const number = Math.floor(Math.random() * 10 + 1);
+reactDOM.render(
+  <div>
+    <h1>Hello my name is {name} </h1>
+    <p>My lucky number is {number}</p>
+  </div>,
+  document.getElementById("root")
+);
