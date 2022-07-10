e536be961e4d00236749020d72ed3e3d71f5b34a
D3N2-A <anmolsharma999937@gmail.com>
2022-07-10 16:42:31 +0530

    Prop from list

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,34 +1,41 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 // <-----------------Setup Vars------------------------->
-const firstBook = {
-  title: "The Psychology of Money",
-  author: "Morgan Housel",
-  image:
-    "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg",
-};
+const books = [
+  {
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg",
+  },
+  {
+    title: "The Power of Your Subconscious Mind",
+    author: "Joseph Murphy",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg",
+  },
+];
 // <-----------------Componenets------------------------->
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        image={firstBook.image}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
+      {books.map((book) => {
+        console.log(book);
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 const Book = (props) => {
+  const { image, title, author } = props.book;
+  console.log(props);
   return (
     <article className="book">
-      <img className="photo" src={props.image} alt="" />
-      <h1 style={{ color: "#232946" }}>{props.title}</h1>
-      <h4 style={{ fontSize: "0.75rem", marginTop: "0.25rem" }}>
-        {props.author}
-      </h4>
+      <img className="photo" src={image} alt="" />
+      <h1 style={{ color: "#232946" }}>{title}</h1>
+      <h4 style={{ fontSize: "0.75rem", marginTop: "0.25rem" }}>{author}</h4>
     </article>
   );
 };
